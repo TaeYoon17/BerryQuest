b74e3954fb133a300a420e5dcd2c20b503d354c5
TaeYoon17 <46375289+TaeYoon17@users.noreply.github.com>
2024-10-11 23:18:20 +0900

    Initial Commit

---
+++ BerryQuest/BerryQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BerryQuestApp.swift
+//  BerryQuest
+//
+//  Created by Greem on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct BerryQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
